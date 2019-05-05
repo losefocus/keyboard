279e016c0685f1b5ed891199a2efc7fe5a142bec
unknown <627562360@qq.com>
2019-05-05 17:59:19 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<style lang="scss" scoped>
+    #app {
+        width: 680px;
+        margin: 20px auto;
+        font-family: Verdana, Sans-Serif; 
+
+        h1 {
+            color: #42b983;
+            font-weight: bold;
+
+        }
+
+        textarea {
+            display: block;
+            width: 100%;
+            min-height: 100px;
+            padding: 0;
+            margin: 20px 0;
+            font-size: 16px;
+        }
+    }
+</style>
+<template>
+    <div id="app">
+        <h1>Just Enter Text:</h1>
+        <textarea type="text" v-model="keyboardText"></textarea>
+        <keyboard @sendText="getText"></keyboard>
+    </div>
+</template>
+
+<script>
+import Keyboard from './components/keyboard'
+
+export default {
+    data() {
+        return {
+            keyboardText:''
+        }
+    },
+
+    components: {
+        Keyboard
+    },
+    methods:{
+        getText(msg){
+            console.log(msg)
+            this.keyboardText = msg
+        }
+    }
+}
+</script>
